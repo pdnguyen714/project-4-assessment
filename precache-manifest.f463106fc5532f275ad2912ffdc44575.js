e9d89ca590396b987916a4820d4d352555908197
Danny <danny@admins-mbp.attlocal.net>
2020-06-02 14:39:43 -0700

    Updates

---
+++ precache-manifest.f463106fc5532f275ad2912ffdc44575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749ebf8b9b6bdd222c1453754e9290b9",
+    "url": "/project-4-assessment/index.html"
+  },
+  {
+    "revision": "c1ce12c95e928b34d5f2",
+    "url": "/project-4-assessment/static/css/main.594a592a.chunk.css"
+  },
+  {
+    "revision": "3357667a71712448bd75",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-4-assessment/static/js/2.2018531e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ce12c95e928b34d5f2",
+    "url": "/project-4-assessment/static/js/main.86d4840e.chunk.js"
+  },
+  {
+    "revision": "11176ca57580c991af7e",
+    "url": "/project-4-assessment/static/js/runtime-main.548f4f76.js"
+  }
+]);
